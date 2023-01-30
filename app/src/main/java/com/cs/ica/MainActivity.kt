b9a5f73027766a179b65dbf130abdf80fd3754e6
cryptoAlgorithm <cryptoalgro@gmail.com>
2023-01-30 22:40:59 +0800

    feat: initial bare-bones commit

---
+++ app/src/main/java/com/cs/ica/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.cs.ica
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cs.ica.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    // Enable these to add menu items to the app abr
+    /* override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    } */
+}
